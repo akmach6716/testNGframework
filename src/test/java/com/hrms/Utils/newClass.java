0bde0cf55aaa54af9e83e7acc98258aa5a623e86
akmach6716 <akmach6716@gmail.com>
2021-01-25 23:39:31 +0530

    some small program added

---
+++ src/test/java/com/hrms/Utils/newClass.java
@@ -0,0 +1,10 @@
+package com.hrms.Utils;
+
+public class newClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("test check 123");
+	}
+
+}
